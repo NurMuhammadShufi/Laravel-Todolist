747cfed5a7748cd731c7291e211ae266f374eb11
Nur Muhammad Shufi <shufimuhammad013@gmail.com>
2023-03-28 03:08:57 +0700

    Create User Service

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\UserServiceProdvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -156,9 +157,9 @@ return [
 
     'providers' => [
 
-        /*
-         * Laravel Framework Service Providers...
-         */
+            /*
+             * Laravel Framework Service Providers...
+             */
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -182,18 +183,19 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        /*
-         * Package Service Providers...
-         */
+            /*
+             * Package Service Providers...
+             */
 
-        /*
-         * Application Service Providers...
-         */
+            /*
+             * Application Service Providers...
+             */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+            // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\UserServiceProdvider::class,
 
     ],
 
